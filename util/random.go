599a15438961f82a1899dd9a91a4cc3518612dd2
Shawgi Hafiz <shawgichan@gmail.com>
2024-02-25 07:25:30 +0400

    testing

---
+++ util/random.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt returns a random int between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString returns a random string of length n
+func RandomString(n int) string {
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+// RandomOwner returns a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoney returns a random money amount
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "GBP", "BTC"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
